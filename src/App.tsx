82b3d9a5b51773a13774dc92a51ede2542edb009
Arya Rajput <rajputarya10@gmail.com>
2025-06-04 13:51:08 +0530

    logs setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto max-w-6xl">
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="backdrop-blur-md shadow-sm">
+            <div className="mx-auto px-6 lg:px-8 h-16 flex items-center justify-between text-white">
+                <h1 className="text-xl font-bold text-gradient hover:scale-105 duration-100 cursor-pointer">Logstack.dev</h1>
+                {/* <ul className="flex space-x-6 font-medium">
+                    <li><a href="#" className="hover:underline">Home</a></li>
+                </ul> */}
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
